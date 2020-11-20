df92d9c568d14acb16ff0895f86b0f36bacef321
erzhukov <erzhukov@i-teco.ru>
2020-11-20 16:25:54 +0300

    task6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
 
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [
+  {
+    type: 'success',
+    message: 'успех',
+  },
+  {
+    type: 'info',
+    message: 'информация',
+  },
+  {
+    type: 'warning',
+    message: 'предупржедение!',
+  },
+  {
+    type: 'danger',
+    message: 'ошибка',
+  },
+  {
+    type: 'primary',
+    message: 'основной месседж',
+  },
+  {
+    type: 'secondary',
+    message: 'второстепенный',
+  },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular10-lecture3-task6';
+  alerts: Alert[];
+
+  constructor() {
+    this.reset();
+  }
+
+  close(alert: Alert): void {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset(): void {
+    this.alerts = Array.from(ALERTS);
+  }
 }
